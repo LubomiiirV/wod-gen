00cfd953ed8155deaf215cf4fdb0f2ff87463b64
vlubosh <72025019+vlubosh@users.noreply.github.com>
2021-09-28 16:16:24 +0200

    first upload
    working code, first style, added 11 workouts

---
+++ app.js
@@ -0,0 +1,433 @@
+var NumberOfWods = 11
+
+var wods = new BuildArray(NumberOfWods)
+
+// Use the following variables to 
+// define your random wods:
+wods[1] = {
+    name: `11.1`,
+    description: `10 Min AMRAP`,
+    wod: `30 DUs
+    15 Power Snatches 75/55 lbs`,
+    tips: `Top 5% score: 338 reps (7 rounds + 23 DUs) DUs unbroken Split snatches to 2-3 sets with quick rest Minimize transition times`
+
+}
+
+wods[2] = {
+    name: `11.2`,
+    description:
+        `15 Min AMRAP`,
+    wod: `9 Deadlifts 155/100 lbs
+        12 HR Push Ups
+        15 Box Jumps 24/20″`,
+    tips: `
+    Top 5% score: 408 reps (11 rounds + 3 HR push ups)
+    – Go unbroken on the deadlifts
+    – Recover on HR push ups, keep steady pace
+    – Get over box jumps quickly
+    – There is no need to rest in this WOD, you just need to keep pushing the pace`
+}
+
+
+wods[3] = {
+    name: `11.3`,
+    description:
+        `5 Min AMRAP`,
+    wod: `Squat Clean 165/110 lbs
+        Jerk 165/110 lbs`,
+    tips: `– Top 5% score: 60 reps (30+30)
+    – Do only singles
+    – Steady pace from the beginning
+    – Use your legs on the push jerk`
+}
+
+wods[4] = {
+    name: `11.4`,
+    description: `10 Min AMRAP`,
+    wod: `60 Bar Facing Burpees
+    30 Overhead Squats 120/90 lbs
+    10 Muscle Ups`,
+    tips: `– Top 5% score: 120 reps (1 round + 20 burpees)
+    – Steady pace on the burpees, step down/up to the burpee if you need to
+    – Try to do overhead squats in a few sets as possible, try 12+10+8 or 18+
+    – Get over muscle ups as quickly as you can, if you need to split sets try 4+3+3 or 3+3+2+2`
+}
+wods[5] = {
+    name: `11.5`,
+    description: `20 Min AMRAP`,
+    wod: `5 Power Cleans 145/100 lbs
+    10 Toes-to-Bars
+    15 Wall Balls 20/14 lbs`,
+    tips: `– Top 5% score: 330 reps (11 rounds)
+    – Single on power cleans
+    – TTB in 2 sets of 6+4
+    – Wall balls unbroken if you feel ok or in sets of 8+7 or 6+5+4
+    – Minimize transition times`
+}
+
+wods[6] = {
+    name: `11.6`,
+    description: `7 Min AMRAP`,
+    wod:`3-6-9-12-15-…
+    Thrusters 100/65 lbs
+    Chest-to-Bar Pull Ups`,
+    tips: `– Top 5% score: 121 reps (13 C2B in round of 18)
+    – Do max of 7 thrusters in 1 set with quick rest between sets
+    – Do max of 4 C2B in 1 set, it is worth to split even the set of 6 C2B to keep moving`
+}
+
+wods[7] = {
+    name: `12.1`,
+    description: `7 Min AMRAP`,
+    wod:`Target Burpees`,
+    tips: `– Top 5% score: 123 reps
+    – Just go and don’t stop, steady pace, keep moving and improve the pace in the last minute`
+}
+
+wods[8] = {
+    name: `12.2`,
+    description: `10 Min AMRAP`,
+    wod:`
+    30 Snatches 75/45 lbs
+    30 Snatches 135/75 lbs
+    30 Snatches 165/100 lbs
+    Max Snatches 210/120 lbs`,
+    tips: `– Top 5% score: 75 reps (15 with 165/100 lbs)
+    – Do 12+10+8 or 6×5 with 75/45 lbs
+    – On heavier weights go for singles with constant rest`
+}
+
+wods[9] = {
+    name: `12.3`,
+    description: `18 Min AMRAP
+    `,
+    wod:`15 Box Jumps 24/20″
+    12 Push Presses 115/75
+    9 Toes-to-Bars`,
+    tips: `– Top 5% score: 367 reps (10 rounds + 7 box jumps)
+    – Rest on the top of the box to recover
+    – Push presses unbroken or in 2 sets of 7+5
+    – Split TTB in the 2 sets of 5+4`
+}
+
+wods[10] = {
+    name: `12.4`,
+    description: `12 Min AMRAP`,
+    wod: `150 Wall Balls 20/14 lbs
+    90 DUs
+    30 Muscle Ups`
+    ,
+    tips: `– Top 5% score: 255 reps (15 muscle ups)
+    – On wall balls try multiple small sets with quick rest like 30 sets of 5 or try large/small set approach, this will allow you to do a lot of wall balls and recover with smaller set 40+10+10+30+10+10+20+20
+    – DUs in 2 or 3 sets, don’t need to go unbroken as you need to recover before MU
+    – One large and multiple small sets on muscle ups`
+}
+
+wods[11] = {
+    name: `12.5`,
+    description: `7 Min AMRAP`,
+    wod: `3-6-9-12-15-…
+    Thrusters 100/65 lbs
+    Chest-to-Bar Pull Ups`,
+    tips: `– Top 5% score: 121 reps (13 C2B in round of 18)
+    – Do max of 7 thrusters in 1 set with quick rest between sets
+    – Do max of 4 C2B in 1 set, it is worth to split even the set of 6 C2B to keep moving
+    `
+}
+
+wods[12] = {
+    name: ` 13.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[13] = {
+    name: `13.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[14] = {
+    name: ` 13.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[15] = {
+    name: ` 13.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[16] = {
+    name: ` 13.5`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[17] = {
+    name: ` 14.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[18] = {
+    name: ` 14.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[19] = {
+    name: ` 14.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[20] = {
+    name: ` 14.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[21] = {
+    name: ` 14.5`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[22] = {
+    name: ` 15.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[23] = {
+    name: ` 15.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[24] = {
+    name: ` 15.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[25] = {
+    name: ` 15.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[26] = {
+    name: ` 15.5`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[28] = {
+    name: ` 16.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[29] = {
+    name: `16.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[30] = {
+    name: `16.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[31] = {
+    name: `16.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[32] = {
+    name: `16.5`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[33] = {
+    name: `17.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[34] = {
+    name: `17.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[35] = {
+    name: ` 17.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[36] = {
+    name: ` 17.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[37] = {
+    name: ` 17.5`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[38] = {
+    name: ` 18.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[39] = {
+    name: ` 18.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[40] = {
+    name: ` 18.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[41] = {
+    name: ` 18.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[42] = {
+    name: ` 18.5`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[43] = {
+    name: ` 19.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[44] = {
+    name: ` 19.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[45] = {
+    name: ` 19.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[46] = {
+    name: ` 19.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[47] = {
+    name: ` 19.5`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[48] = {
+    name: ` 20.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[49] = {
+    name: `20.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[50] = {
+    name: `20.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[51] = {
+    name: `20.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[52] = {
+    name: `20.5`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[53] = {
+    name: `21.1`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[54] = {
+    name: `21.2`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[55] = {
+    name: `21.3`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+wods[56] = {
+    name: `21.4`,
+    description: ``,
+    wod: ``,
+    tips: ``
+}
+
+// const rows = text.split('/n')
+
+function BuildArray(size) {
+    this.length = size
+    for (var i = 1; i <= size; i++) {
+        this[i] = null
+    }
+    return this;
+}
+
+function PickRandomWod(frm) {
+    // Generate a random number between 1 and NumberOfWods
+    var rnd = Math.ceil(Math.random() * NumberOfWods)
+
+    //display wods inside div
+    document.getElementById("wod-name").textContent = wods[rnd].name;
+    document.getElementById("wod-description").textContent = wods[rnd].description;
+    // document.getElementById("wod-wod").innerHTML = wods[rnd].wod;
+    document.getElementById("wod-wod").innerHTML = wods[rnd].wod.replace(/(\r\n|\n|\r)/gm, "<br>");
+    document.getElementById("wod-tips").textContent = wods[rnd].tips;
+
+}
+
+
+
+
+
+
+
+
+
